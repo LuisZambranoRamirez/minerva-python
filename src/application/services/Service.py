be8019fb41327d3566f3cc3993000c4c3e6fbd98
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-09 01:08:09 -0500

    service

---
+++ src/application/services/Service.py
@@ -0,0 +1,56 @@
+from abc import ABC
+
+from domain.repositories.UserRepository import UserRepository
+from domain.constants.Permission import Permission
+from domain.constants.Role import Role
+from domain.entities.userAction.UserAction import UserAction
+from domain.exceptions.DomainException import DomainException
+from domain.interfaces.Id import Id
+from domain.valueObject.id.UserName import UserName
+
+
+class Service(ABC):
+
+    def __init__(
+        self,
+        user_role: Role,
+        user_name: UserName,
+        user_repository: UserRepository,
+    ):
+        if user_role is None:
+            raise RuntimeError(
+                "El usuario debe tener un rol, no puede ser nulo."
+            )
+
+        if user_name is None:
+            raise RuntimeError(
+                "El nombre del usuario no puede ser nulo."
+            )
+
+        self._user_role = user_role
+        self._user_name = user_name
+        self._user_repository = user_repository
+
+    # Como segunda barrera de defensa aquí también podría validarse
+    # que el rol tenga permiso para ejecutar la acción.
+    def _register_user_action(
+        self,
+        permission: Permission,
+        entity_id: Id,
+    ) -> None:
+
+        try:
+            self._user_repository.save_user_action(
+                UserAction(
+                    self._user_name,
+                    permission,
+                    entity_id,
+                )
+            )
+
+        except DomainException as e:
+            raise RuntimeError(str(e)) from e
+
+    @property
+    def user_role(self) -> Role:
+        return self._user_role

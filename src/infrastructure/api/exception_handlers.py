30cdd2befdb2a689b6c2a48bf4bfaf39f6149e18
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-09 19:06:52 -0500

    handlers

--- src/infrastructure/api/exception_handlers.py
+++ src/infrastructure/api/exception_handlers.py
@@ -1,19 +1,30 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from domain.exceptions.UnauthorizedActionException import (
-    UnauthorizedActionException,
-)
+from domain.exceptions.UnauthorizedActionException import UnauthorizedActionException
+
 
 
 def register_exception_handlers(app: FastAPI):
 
     @app.exception_handler(UnauthorizedActionException)
-    async def unauthorized_handler(
-        request: Request,
-        exc: UnauthorizedActionException,
-    ):
+    async def unauthorized_handler(request: Request, exc: UnauthorizedActionException):
+        print(exc)
+
         return JSONResponse(
             status_code=403,
-            content={"detail": str(exc)},
+            content={
+                "message": str(exc)
+            }
+        )
+
+    @app.exception_handler(Exception)
+    async def global_exception_handler(request: Request, exc: Exception):
+        print(exc)
+
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "Error interno del servidor"
+            }
         )

a42496698f077d52ce3bda86bb3835a196e7bf59
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-10 00:51:19 -0500

    depnedencies

--- src/infrastructure/api/dependencies.py
+++ src/infrastructure/api/dependencies.py
@@ -1,36 +1,35 @@
-# infra/api/dependencies.py
+from application.services.UserService import UserService
+from domain.interfaces.PasswordHasher import PasswordHasher
+from infrastructure.adapter.repository.SqlAlchemyProductRepository import SqlAlchemyProductRepository
+from infrastructure.adapter.repository.SqlAlchemySaleRepository import SqlAlchemySaleRepository
+from infrastructure.adapter.repository.SqlAlchemySupplierRepository import SqlAlchemySupplierRepository
+from domain.repositories.SupplierRepository import SupplierRepository
+from domain.repositories.SaleRepository import SaleRepository
+from domain.repositories.ProductRepository import ProductRepository
+from domain.repositories.CustomerRepository import CustomerRepository
+from domain.repositories.UserRepository import UserRepository
+from infrastructure.adapter.BcryptPasswordHasher import BcryptPasswordHasher
+from infrastructure.adapter.repository.SqlAlchemyUserRepository import SqlAlchemyUserRepository
+from infrastructure.persistence.session import SessionLocal
+from infrastructure.adapter.repository.SqlAlchemyCustomerRepository import SqlAlchemyCustomerRepository
 
-from fastapi import Depends
+def get_customer_repository() -> CustomerRepository:
+    return SqlAlchemyCustomerRepository(SessionLocal())
 
-from application.services.CustomerService import CustomerService
+def get_product_repository() -> ProductRepository:
+    return SqlAlchemyProductRepository(SessionLocal())
 
+def get_sale_repository() -> SaleRepository:
+    return SqlAlchemySaleRepository(SessionLocal())
 
-def get_customer_service() -> CustomerService:
-    # Crear repositorios
-    # Obtener usuario autenticado
-    # Construir CustomerService
+def get_supplier_repository() -> SupplierRepository:
+    return SqlAlchemySupplierRepository(SessionLocal())
 
-    return CustomerService(
-        user_role=...,
-        user_name=...,
-        user_repository=...,
-        customer_repository=...,
-    )
+def get_user_repository() -> UserRepository:
+    return SqlAlchemyUserRepository(SessionLocal())
 
-def get_sale_service() -> SaleService:
-    return SaleService(
-        user_role=...,
-        user_name=...,
-        user_repository=...,
-        sale_repository=...,
-        customer_repository=...,
-        product_repository=...,
-    )
-
-# infra/api/dependencies.py
+def get_password_hasher() -> PasswordHasher:
+    return BcryptPasswordHasher()
 
 def get_user_service() -> UserService:
-    return UserService(
-        user_repository=...,
-        password_hasher=...,
-    )
+    return UserService(get_user_repository(), get_password_hasher())
